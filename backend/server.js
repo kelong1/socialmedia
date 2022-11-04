51d9280e74a2ca5d8bcf771f1e31237b17d60e1e
kelong1 <kelongzac@gmail.com>
2022-11-04 11:25:10 +0300

    to deploy

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,13 @@ app.use("/app/users",UserUrls)
 app.use("/app/posts",PostUrls)
 
 app.listen(process.env.PORT,()=>console.log("Port is running at 9000"))
-
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+//     app.get('*', (req, res) =>
+//       res.sendFile(
+//         path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//       )
+//     );
+//   } 
 
